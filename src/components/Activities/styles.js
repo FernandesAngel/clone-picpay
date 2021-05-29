be0d2d394097ee5e9abe102999dcc58d10d41854
Angel Fernandes <angelfernandes07@hotmail.com>
2021-05-29 14:46:53 -0300

    Suggestions and Activities component

---
+++ src/components/Activities/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    margin-top: 25px;
+    padding: 0 16px;
+`;
+
+export const Header = styled.View`
+`;
+
+export const Title = styled.Text`
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+`;
+
+export const Card = styled.View`
+    background: #1e222b;
+    border-radius: 8px;
+    margin-top: 20px;
+    padding: 15px;
+`;
+export const CardHeader = styled.View`
+`;
+export const Avatar = styled.Image`
+`;
+export const Description = styled.Text`
+`;
+export const Bold = styled.Text`
+`;
